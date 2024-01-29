aa24efd17d95f777485ec722e7d511350a5430ac
Pedro Alarcon <pedroa54@icloud.com>
2024-01-28 23:08:46 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance) {
+  if (rideDistance <= 400) {
+    return 'This one is on me!';
+  } else if (rideDistance > 400 && rideDistance <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (rideDistance > 2000 && rideDistance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  // Check if the city is NYC
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+module.exports = {
+  ternaryCheckCity
+};
+
+function switchOnCharmFromTip(tip) {
+  let response;
+
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+  }
+
+  return response;
 }
+
+module.exports = {
+  switchOnCharmFromTip
+};
